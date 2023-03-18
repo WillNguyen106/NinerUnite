42ae10c59838dc72c41e49b96dc567b488e2a13a
Quang Ho <qho2@uncc.edu>
2023-03-18 14:10:04 -0400

    New for image upload

--- Sprint-2/routes/bookRoutes.js
+++ Sprint-2/routes/bookRoutes.js
@@ -17,7 +17,7 @@ router.get('/', controller.index);
 router.get('/new', controller.new);
 
 //POST /books: Post a new textbook for selling
-router.post('/', controller.create);
+router.post('/', fileUpload,controller.create);
 
 //GET /books/:id: send details of textbook product indentified by id
 router.get('/:id', controller.show);
@@ -35,7 +35,7 @@ router.post('/search', controller.search);
 router.get('/:id/edit', controller.edit);
 
 //PUT /books/:id: update the textbook post identified by id
-router.put('/:id', controller.update);
+router.put('/:id', fileUpload, controller.update);
 
 //DELETE /books/:id: delete textbook identified by id
 router.delete('/:id', controller.delete);
