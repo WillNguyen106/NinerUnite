ee27a573ff19c3581248ce958879f32288dd5a5b
[Willnguyen106] <qnguye33@uncc.edu>
2023-04-22 14:02:37 -0400

    updated by Will

--- Sprint-3/routes/bookRoutes.js
+++ Sprint-3/routes/bookRoutes.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const controllerBook = require('../controllers/bookController');
 const {fileUpload} = require('../middlewares/fileUpload');
-<<<<<<< Updated upstream
 const {isLoggedIn, isUserBookPost} = require('../middlewares/auth');
-=======
-const {isLoggedIn, isUserPost} = require('../middlewares/auth');
->>>>>>> Stashed changes
 const {validateId} = require('../middlewares/validator');
 
 
@@ -38,7 +34,6 @@ router.get('/:id',validateId, controllerBook.show);
 // */
 
 //GET /books/:id/edit: send HTML form for editing an existing textbook post
-<<<<<<< Updated upstream
 router.get('/:id/edit',validateId,isLoggedIn,isUserBookPost,controllerBook.edit);
 
 //PUT /books/:id: update the textbook post identified by id
@@ -46,15 +41,6 @@ router.put('/:id',validateId,isLoggedIn,isUserBookPost,fileUpload, controllerBoo
 
 //DELETE /books/:id: delete textbook identified by id
 router.delete('/:id',validateId,isLoggedIn,isUserBookPost,controllerBook.delete);
-=======
-router.get('/:id/edit',validateId,isLoggedIn,isUserPost,controllerBook.edit);
-
-//PUT /books/:id: update the textbook post identified by id
-router.put('/:id',validateId,isLoggedIn,isUserPost,fileUpload, controllerBook.update);
-
-//DELETE /books/:id: delete textbook identified by id
-router.delete('/:id',validateId,isLoggedIn,isUserPost,controllerBook.delete);
->>>>>>> Stashed changes
 
 
 module.exports = router;
